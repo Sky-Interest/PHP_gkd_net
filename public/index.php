5478eac8736a7dd4967d2539a53ee55a34148b62
Sky_Interest <841102500@qq.com>
2024-05-15 19:14:31 +0800

    14. 修改 Apache 配置文件以改变服务器跟路径对应的目录

--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
 <?php 
     require '../helpers.php';
-    loadView('home');
+    // loadView('home');
+    $routes = [
+        '/' => 'controller/home.php',
+        '/listings' => 'controller/listings/index.php',
+        '/listings/create' => 'controller/listings/create.php',
+        '404' => 'controller/listings/404.php',
+    ];
+
+    $uri = $_SERVER['REQUEST_URI'];
+    inspectAndDie($uri);
