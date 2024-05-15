828a66077971878b1a735f3270c5100e12e8ac7c
Sky_Interest <841102500@qq.com>
2024-05-15 17:14:19 +0800

    8. 创建loadView函数

--- helpers.php
+++ helpers.php
@@ -8,4 +8,13 @@
 // - **功能**：通过拼接**`basePath`**函数返回的路径和传入的视图文件名来构建完整的文件路径，然后使用**`require`**语句加载指定的视图文件。
         require basePath("views/partials/{$name}.php");
     }
+
+    function loadView($name){
+        $viewPath = basePath("views/{$name}.view.php");
+        if(file_exists($viewPath)){
+            require $viewPath;
+        }else{
+            echo"{$viewPath}视图不存在!";
+        }
+    }
 ?>
