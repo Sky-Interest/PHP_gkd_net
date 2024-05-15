c202e9f7f79ac9054ecb7093e6e7ae61395f8b5a
Sky_Interest <841102500@qq.com>
2024-05-15 21:08:49 +0800

    17. 创建工作列表控制器

--- views/home.view.php
+++ views/home.view.php
@@ -77,7 +77,7 @@
             </div>
           </div>
         </div>
-        <a href="listings.html" class="block text-xl text-center">
+        <a href="/listings" class="block text-xl text-center">
           <i class="fa fa-arrow-alt-circle-right"></i>
           查看所有实习
         </a>
