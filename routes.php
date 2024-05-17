a6d22cc3d1a2770e7713f57a2460b9d4b8633cf1
Sky_Interest <841102500@qq.com>
2024-05-17 19:07:49 +0800

    3.10. 新增 ListingController

--- routes.php
+++ routes.php
@@ -5,3 +5,8 @@ $router->addGet('/','HomeController@index');
 // $router->addGet('/listings','controllers/listings/index.php');
 // $router->addGet('/listings/create','controllers/listings/create.php');
 // $router->addGet('/listing','controllers/listings/show.php');
+
+
+$router->addGet('/listings','ListingController@index');
+$router->addGet('/listings/create','ListingController@create');
+$router->addGet('/listing','ListingController@show');
